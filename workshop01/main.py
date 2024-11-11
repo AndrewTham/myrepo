7f97d549cbd3c0d1373fa95d73a0fb94aa9be19d
Bear <145958534+bigbear555@users.noreply.github.com>
2024-11-11 22:29:39 +0800

    Added landing page

---
+++ workshop01/main.py
@@ -0,0 +1,24 @@
+import json
+from random import choice
+
+from flask import Flask, render_template
+
+app = Flask(__name__)
+
+with open("random.json", "r", encoding="utf-8") as f:
+    quotes = json.load(f)
+
+
+@app.route("/")
+def home():
+    image_url = "https://thumbs.dreamstime.com/b/fortune-cookie-29364274.jpg"
+    repo_url = "https://github.com/AndrewTham/myrepo"
+    quote = choice(quotes)
+
+    return render_template(
+        "index.html", image_url=image_url, repo_url=repo_url, quote=quote
+    )
+
+
+if __name__ == "__main__":
+    app.run(debug=True)
